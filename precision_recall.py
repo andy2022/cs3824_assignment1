332f49d2b73cc8e443cda47abf578e82c67ad51e
Andy Athawale <andy.athawale@gmail.com>
2022-09-23 11:03:54 -0400

    Changed preallocation of list to empty list

--- precision_recall.py
+++ precision_recall.py
@@ -1,6 +1,6 @@
 ## Function to compute data for generating precision-recall curve
 
-def func(ranked_edges, g):
+def generate(ranked_edges, g):
     '''
     For each rank in the ranked-edges file, check if all edges appear in the
     corresponding graph structure (provided as parameter).
@@ -8,11 +8,13 @@ def func(ranked_edges, g):
     g: corresponding graph structure from part 1
     '''
     true_positives = 0
-    total_edges = 0 # Maintain count of total edges in ranked-edges file
-    precision = [0] * (list(ranked_edges)[-1]+1)
-    recall = [0] * (list(ranked_edges)[-1]+1)
+    current_edge = 0
+    #precision = [0] * (len(ranked_edges)*100)
+    #recall = [0] * (len(ranked_edges)*100)
+    precision = []
+    recall = []
 
-    for rank in ranked_edges.keys():
+    for rank in [i for i in ranked_edges.keys() if len(ranked_edges[i]) > 0]:
 
         # Iterate through list of all edges in this rank
         for edge in ranked_edges[rank]:
@@ -20,10 +22,10 @@ def func(ranked_edges, g):
             # If current edge exists in graph, increment true positives
             if g.has_edge(*edge):
                 true_positives += 1
-            total_edges += 1
+            current_edge += 1 # Increment current edge number
 
-        # Calculate precision and recall for this rank
-        precision[rank] = true_positives / rank
-        recall[rank] = true_positives / list(ranked_edges)[-1]
+            # Calculate precision and recall for this edge
+            precision.append(true_positives / current_edge)
+            recall.append(true_positives / g.number_of_edges())
 
     return precision, recall
